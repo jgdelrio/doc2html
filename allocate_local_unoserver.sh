6f5084110e2d08831ec14415ade0bd89f319e9e5
Jose del Rio <jgregrs@yahoo.es>
2024-08-05 12:39:41 +0200

    Allocating package files

--- allocate_local_unoserver.sh
+++ allocate_local_unoserver.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-if [ ! -d "environments" ]; then
-    mkdir environments
-    echo "Created 'environments' directory."
-
-    virtualenv --python=/usr/bin/python3 --system-site-packages environments/virtenv
-    echo "Allocated system packages"
-
-    ./environments/virtenv/bin/pip install unoserver
-    echo "Unoserver has been installed in the local environment"
-else
-    echo "'environments' directory already exists."
-fi
+./doc2html/allocate_unoserver.sh
